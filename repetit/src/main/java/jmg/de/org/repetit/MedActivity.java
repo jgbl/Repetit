7a35bc4784b65b421be39ebec9d16a728706db9f
jhmgbl <jhmgbl@gmail.com>
2017-08-31 17:58:22 +0200

    treeview

--- repetit/src/main/java/jmg/de/org/repetit/MedActivity.java
+++ repetit/src/main/java/jmg/de/org/repetit/MedActivity.java
@@ -2,14 +2,36 @@ package jmg.de.org.repetit;
 
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import me.texy.treeview.TreeNode;
+import me.texy.treeview.base.BaseNodeViewBinder;
 
 /**
  * Created by hmnatalie on 29.08.17.
  */
 
 public class MedActivity extends Fragment {
-public final  static int fragID = 1;
+    public final static int fragID = 1;
 
     public MainActivity _main;
+}
+public class FirstLevelNodeViewBinder extends BaseNodeViewBinder {
+    TextView textView;
+    public FirstLevelNodeViewBinder(View itemView) {
+        super(itemView);
+        textView = (TextView) itemView.findViewById(R.id.node_name_view)
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.item_first_level;
+    }
+
+    @Override
+    public void bindView(TreeNode treeNode) {
+        textView.setText(treeNode.getValue().toString());
+    }
 }
 
